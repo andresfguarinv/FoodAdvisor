b007b83a75e8b1d8f895fa57f7fce19d00b2ed06
andresfguarinv <andresfguarinv@gmail.com>
2019-10-03 21:11:51 -0500

    23. Validando el formulario de registro

--- app/screens/MyAccount/Register.js
+++ app/screens/MyAccount/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 // Importar formulario
 import { RegisterStruct, RegisterOptions } from '../../forms/Register'
 import t from 'tcomb-form-native'
@@ -12,17 +13,49 @@ export default class Register extends Component {
         super();
         this.state = {
             registerStruct: RegisterStruct,
-            registerOptions: RegisterOptions
+            registerOptions: RegisterOptions,
+            // 5. Para asignar la información del formulario se deben agregar al state 
+            formData: {}
         };
     }
 
+    // 7. Función para el boton del login que valida el formulario 
+    register = () => {
+        const { password, passwordConfirmation } = this.state.formData;
+        if (password === passwordConfirmation) {
+            const validate = this.refs.registerForm.getValue(); // Valida el formulario al traer los valores
+            if (validate) {
+                console.log(this.state.formData);
+            } else {
+                console.log(`Formulario Inválido`);
+            }
+        }
+        else {
+            console.log(`Las contraseñas no son iguales`);
+        }
+    }
+
+    // 6. Funcion que se debe asignar al formulario para que cada vez que se escriba almanecenar la información en nuestro objeto
+    onChangeFormRegister = (formValue) => {
+        this.setState({
+            formData: formValue
+        });
+        // console.log(this.state.formData);
+    }
+
     render() {
-
         const { registerStruct, registerOptions } = this.state;
-
         return (
             <View style={styles.viewBody}>
-                <Form ref="registerForm" type={registerStruct} options={registerOptions}></Form>
+                <Form
+                    ref="registerForm"
+                    type={registerStruct}
+                    options={registerOptions}
+                    value={this.state.formData}
+                    onChange={(formValue) => this.onChangeFormRegister(formValue)} />
+                <Button
+                    title="Unirse"
+                    onPress={() => this.register()} />
             </View>
         );
     }
