487825673eea06b49b8f1eea22baf3bdf282a17b
andresfguarinv <andresfguarinv@gmail.com>
2019-11-12 20:23:12 -0500

    31-32-33. Creando y validando el formulario de login

--- app/screens/MyAccount/Login.js
+++ app/screens/MyAccount/Login.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
-import { Image } from 'react-native-elements'
-
+import { Image, Button } from 'react-native-elements'
+import { LoginStruct, LoginOptions } from '../../forms/Login'
+import t from 'tcomb-form-native'
+const Form = t.form.Form;
 
 export default class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            loginStruct: LoginStruct,
+            loginOptions: LoginOptions,
+            loginData: {},
+            loginErrorMessage: ''
+        }
+    }
+
+    onChangeFormLogin = (formValue) => {
+        this.setState({
+            loginData: formValue
+        })
+    }
+
+    login = () => {
+        this.setState({ loginErrorMessage: '' });
+        const validate = this.refs.loginForm.getValue();
+        if (!validate) {
+            this.setState({ loginErrorMessage: 'Los datos del formulario son erroneos' });
+            return;
+        }
+        console.log(this.state.loginData);
+    }
+
     render() {
+        const { loginStruct, loginOptions, loginErrorMessage } = this.state;
         return (
             <View style={styles.viewBody}>
                 <Image
@@ -12,7 +42,22 @@ export default class Login extends Component {
                     style={styles.logo}
                     PlaceholderContent={<ActivityIndicator />}
                     resizeMode='contain'
+                    containerStyle={styles.containerLogo}
                 />
+                <View style={styles.viewForm}>
+                    <Form
+                        ref='loginForm'
+                        type={loginStruct}
+                        options={loginOptions}
+                        value={this.state.loginData}
+                        onChange={(formValue) => this.onChangeFormLogin(formValue)}
+                    />
+                    <Button buttonStyle={styles.buttonLogin}
+                        title='Entrar'
+                        onPress={() => this.login()}
+                    />
+                    <Text style={styles.loginErrorMessage}>{loginErrorMessage}</Text>
+                </View>
             </View>
         )
     }
@@ -23,11 +68,27 @@ const styles = StyleSheet.create({
         flex: 1,
         marginLeft: 40,
         marginRight: 40,
-        marginTop: 40,
-        alignItems: 'center',
+        marginTop: 40
     },
     logo: {
         width: 300,
         height: 150
+    },
+    viewForm: {
+        marginTop: 50
+    },
+    buttonLogin: {
+        backgroundColor: '#00a680',
+        marginTop: 20,
+        marginLeft: 10,
+        marginRight: 10
+    },
+    containerLogo: {
+        alignItems: 'center'
+    },
+    loginErrorMessage: {
+        color: '#f00',
+        textAlign: 'center',
+        marginTop: 20
     }
 })
