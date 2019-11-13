144f9875a9e006618636df5089c34d899a6a2a30
andresfguarinv <andresfguarinv@gmail.com>
2019-11-12 21:39:33 -0500

    34. Generando login contra firebase

--- app/screens/MyAccount/Login.js
+++ app/screens/MyAccount/Login.js
@@ -4,6 +4,8 @@ import { Image, Button } from 'react-native-elements'
 import { LoginStruct, LoginOptions } from '../../forms/Login'
 import t from 'tcomb-form-native'
 const Form = t.form.Form;
+import * as firebase from 'firebase'
+import Toast, { DURATION } from 'react-native-easy-toast'
 
 export default class Login extends Component {
 
@@ -30,7 +32,24 @@ export default class Login extends Component {
             this.setState({ loginErrorMessage: 'Los datos del formulario son erroneos' });
             return;
         }
-        console.log(this.state.loginData);
+        firebase.auth().signInWithEmailAndPassword(validate.email, validate.password).then(() => {
+            this.refs.toastLogin.show('Login correcto', 200, () => {
+                this.props.navigation.goBack();
+            })
+        }).catch((error) => {
+            let errorMessage;
+            console.log(error.code);
+            switch (error.code) {
+                case 'auth/wrong-password':
+                case 'auth/user-not-found':
+                    errorMessage = 'Credenciales incorrectas';
+                    break;
+                default:
+                    errorMessage = 'Login incorrecto';
+                    break;
+            }
+            this.refs.toastLogin.show(errorMessage, 2500);
+        });
     }
 
     render() {
@@ -58,6 +77,15 @@ export default class Login extends Component {
                     />
                     <Text style={styles.loginErrorMessage}>{loginErrorMessage}</Text>
                 </View>
+                <Toast
+                    ref='toastLogin'
+                    position='bottom'
+                    positionValue={400}
+                    fadeInDuration={1000}
+                    fadeOutDuration={1000}
+                    opacity={0.8}
+                    textStyle={{ color: '#fff' }}
+                />
             </View>
         )
     }
--- app/screens/MyAccount/MyAccount.js
+++ app/screens/MyAccount/MyAccount.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements'
+import * as firebase from 'firebase'
 
 export default class MyAccount extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            login: false
+        }
+    }
+
+    // Funcion que se ejecuta luego del constructor y luego del render
+    async componentDidMount() {
+        await firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({ login: true });
+            }
+        });
+    }
 
     goToScreen = nameScreen => {
-        console.log(nameScreen);
         this.props.navigation.navigate(nameScreen);
     }
 
     render() {
+        const { login } = this.state;
+        if (login) {
+            return (
+                <Text>Estas logueado</Text>
+            );
+        }
         return (
             <View style={styles.viewBody}>
                 <Text>My Account Screen</Text>
